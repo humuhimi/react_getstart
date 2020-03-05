a206d18def5291cb5f5691da11fe4a06325b1d70
humuhimi <masa19951009@gmail.com>
2020-03-05 17:15:12 +0800

    please check if not sure setState

--- src/index.js
+++ src/index.js
@@ -10,9 +10,23 @@ class App extends React.Component {
         super()
         this.state = {
             answer: "Yes",
-            isLoggedIn: true
+            isLoggedIn: true,
+            count: 0
         }
+        this.handleClick = this.handleClick.bind(this)
     }
+    handleClick() {
+        console.log('clicked')
+        // setStateを使うには関数を元のクラスにバインドする必要がある
+        // それによりsetStateを持ってこれる
+        // 中に関数を定義する
+        this.setState(prevState => {
+                return {
+                    count : prevState.count + 1
+                }
+        })
+    }
+
     render() {
         let wordDisplay
         if (this.state.isLoggedIn === true) {
@@ -22,8 +36,11 @@ class App extends React.Component {
         }
         return (
             <div>
+                <img onMouseOver={() => console.log("hovered!!")} src="https://picsum.photos/seed/picsum/200/300" alt="some_moutain"/>
                 <h1>about state: {this.state.answer} </h1>
                 <h2>you are logged {wordDisplay}</h2>
+                <h1><strong>{ this.state.count } </strong></h1>
+                <button onClick={this.handleClick}>Click here</button>
             </div>
         )
     }
